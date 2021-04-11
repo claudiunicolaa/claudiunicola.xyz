015e29c6a979407d07f3e5c8ddcf9e5a311e57e1
Claudiu Nicola <claudiunicola96@gmail.com>
2021-04-11 19:11:06 +0300

    update actions

--- generate.sh
+++ generate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # json --> html
-resume export index.html --theme classy
+#resume export index.html --theme classy
+./node_modules/.bin/resume export index.html --theme classy
 
 # http --> https
 sed -i '' -e 's|http://|https://|g' index.html

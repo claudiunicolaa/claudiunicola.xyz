1b3b90bb112b4469c2f97bb5ca64f7f15e927fce
Claudiu Nicola <claudiunicola96@gmail.com>
2021-04-11 19:17:34 +0300

    update content

--- generate.sh
+++ generate.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # json --> html
-#resume export index.html --theme classy
-./node_modules/.bin/resume export index.html --theme classy
+resume export index.html --theme classy
 
 # http --> https
 sed -i '' -e 's|http://|https://|g' index.html
+
+# push
+git add .
+git commit -m 'update content'
+git push origin master
